f757f6ea7b1480d361469aa1061527b89db58a07
subinium <subinium@gmail.com>
2021-01-12 13:59:17 +0900

    step02 clear

---
+++ steps/step02.py
@@ -0,0 +1,26 @@
+import numpy as np
+from step01 import Variable
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x**2
+
+
+x = Variable(np.array(10))
+f = Square()
+y = f(x)
+
+print(type(y))
+print(y.data)
